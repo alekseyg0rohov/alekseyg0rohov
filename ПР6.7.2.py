98263d1234b280783b2ca1c2f9f467e892accdb8
alekseyg0rohov <alekseyg0rohov@yandex.ru>
2024-10-22 21:28:50 +0300

    ПР6.7.2.py

--- ПР6.7.2.py
+++ ПР6.7.2.py
@@ -1,13 +1,11 @@
-numbers=[]
-for i in range(10):
-    numbers.append(int(input()))
-mx=max(numbers)
-mn=min(numbers)
-for i in range(len(numbers)):
-    if numbers[i]==mx:
-        numbers[i]=mn
-    elif numbers[i]==mn:
-        numbers[i]=mx
-print(numbers)
-    
+s= input().split()
+s2=[]
+for c in s:
+    s2.append(int(c))
+mx=max(s2)
+mn=min(s2)
+smi=s2.index(mx)
+sni=s2.index(mn)
+s2[sni], s2[smi] = s2[smi], s2[sni]
+print(*s2)
     

d133a63479ed7b80fa10f0d587adc4e4817a15f9
alekseyg0rohov <alekseyg0rohov@yandex.ru>
2024-10-22 21:40:31 +0300

    ПР4-9.py

--- ПР4.9.py
+++ ПР4.9.py
@@ -1,15 +1,14 @@
-#9
-n = int(input())
-def f(n):
-    if n <= 0:
+a = int(input())
+def f(a):
+    if a <= 0:
         return 0
-    elif n == 1:
+    elif a == 1:
         return 1
-    a, b = (1, 1)
-    t = a + b
-    for i in range(2, n):
-        a, b = b, a + b
-        t += b
+    f, g = (1, 1)
+    t = f + g
+    for i in range(2, a):
+        f, g = g, f + g
+        t += g
     return t
-r = f(n)г
+r = f(a)г
 print(r)

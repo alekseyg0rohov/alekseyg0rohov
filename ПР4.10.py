89204251e14d6429a34f571fa79e6199e5a532bc
alekseyg0rohov <alekseyg0rohov@yandex.ru>
2024-10-22 21:41:58 +0300

    ПР4-10.py

--- ПР4.10.py
+++ ПР4.10.py
@@ -1,15 +1,13 @@
-#10
-N = int(input())
-k = int(input())
-def f(N, k):
-    if N <= 0 or k <= 0:
+a,b = int(input())
+def f(a, b):
+    if a <= 0 or b <= 0:
         return 0
-    a, b = 0, 1
+    j, f = 0, 1
     s = 0
-    for i in range(1, k + N):
-        if i >= k:
-            s += a
-        a, b = b, a + b
+    for i in range(1, b + a):
+        if i >= b:
+            s += j
+        j, f = f, j + f
     return s
-r = f(N, k)
+r = f(a, b)
 print(r)

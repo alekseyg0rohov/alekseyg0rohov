54f1b527f823c85fadfafe323e0cedfbdb937fc3
alekseyg0rohov <alekseyg0rohov@yandex.ru>
2024-10-23 10:10:16 +0300

    ПР7-9.1.py

--- ПР7-9.1.py
+++ ПР7-9.1.py
@@ -1,9 +1,9 @@
 a = int(input())
-d = 0
+total = 0
 def s(n):
     return sum(int(a) for a in str(n))
 while a > 0:
     a -= s(a)
     if a > 0:
-        d += 1
-print(d+1)
+        total += 1
+print(total+1)
